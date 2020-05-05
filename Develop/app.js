6518e6e846b5b6bcb41340c4eea6c9a9e9407bed
OrbmasWin <sambrostudent@gmail.com>
2020-05-05 17:21:16 +0800

    created app file and added express

---
+++ Develop/app.js
@@ -0,0 +1,18 @@
+// call express
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// create a port to run the server 
+const port = process.env.port || 3000;
+
+// create listen
+app.listen(port, function() {
+    console.log(`app is listening at ${port}`)
+})
+
+// serve the homepage
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+})
+
